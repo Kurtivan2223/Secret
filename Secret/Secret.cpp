1b879f156ad05665c65e04ec6417d9a13126c604
Kurtivan2223 <105442868+Kurtivan2223@users.noreply.github.com>
2024-02-07 13:55:23 +0800

    Update Secret.cpp

--- Secret/Secret.cpp
+++ Secret/Secret.cpp
@@ -10,12 +10,13 @@ int main()
     Logs.Logging(LOG_LEVEL_INFO, "App is Starting");
     Logs.Logging(LOG_LEVEL_INFO, "Setting Configuration");
     Configuration.Configure();
-
+#ifdef _DEBUG
     Logs.Logging(LOG_LEVEL_DEBUG, "HOST: " + Configuration._db._host);
     Logs.Logging(LOG_LEVEL_DEBUG, "USERNAME: " + Configuration._db._user);
     Logs.Logging(LOG_LEVEL_DEBUG, "PASSWORD: " + Configuration._db._pwd);
     Logs.Logging(LOG_LEVEL_DEBUG, "DBNAME: " + Configuration._db._dbname);
     Logs.Logging(LOG_LEVEL_DEBUG, "PORT: " + Configuration._db._port);
+#endif
 
     Logs.Logging(LOG_LEVEL_INFO, "Connecting To the Database");
 
